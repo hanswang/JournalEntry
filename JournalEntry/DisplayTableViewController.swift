36bb35726864719e2f619e33f4f5bf1facba3455
Hans Wang <hanswang2012@gmail.com>
2018-06-13 15:29:49 +1000

    add display view

--- JournalEntry/DisplayTableViewController.swift
+++ JournalEntry/DisplayTableViewController.swift
@@ -11,4 +11,35 @@ import UIKit
 class DisplayTableViewController: UITableViewController {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var items: [Item] = []
+    
+    func fetchData() -> Void {
+        do {
+            items = try context.fetch(Item.fetchRequest())
+            print(items)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("Cannot fetch any data")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchData()
+    }
+}
+
+
+extension DisplayTableViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = items[indexPath.row].name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
 }
